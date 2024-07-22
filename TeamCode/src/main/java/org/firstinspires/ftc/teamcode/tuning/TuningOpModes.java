5f5687a10fd31ba7cdf6493927186cae36411466
Ryan Brott <rcbrott@gmail.com>
2024-07-21 21:55:34 -0700

    add initial program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TuningOpModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TuningOpModes.java
@@ -22,10 +22,10 @@ import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
 
 import org.firstinspires.ftc.robotcore.internal.opmode.OpModeMeta;
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.TankDrive;
-import org.firstinspires.ftc.teamcode.ThreeDeadWheelLocalizer;
-import org.firstinspires.ftc.teamcode.TwoDeadWheelLocalizer;
+import org.firstinspires.ftc.teamcode.RR.MecanumDrive;
+import org.firstinspires.ftc.teamcode.RR.TankDrive;
+import org.firstinspires.ftc.teamcode.RR.ThreeDeadWheelLocalizer;
+import org.firstinspires.ftc.teamcode.RR.TwoDeadWheelLocalizer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
